426709d64740d1607b7169c0bf2b594eea679841
MOAalpha <mohammed-amine.gherras@ensea.fr>
2026-09-18 17:32:34 +0200

    Ajout Dossier de Ressources

---
+++ Ressources/Ressources.h
@@ -0,0 +1,10 @@
+//
+// Created by mathe on 18/09/2026.
+//
+
+#ifndef PALWORD_WISH_RESSOURCES_H
+#define PALWORD_WISH_RESSOURCES_H
+
+#define MOADEX_FILE "../background/pokedex.csv"
+
+#endif //PALWORD_WISH_RESSOURCES_H
